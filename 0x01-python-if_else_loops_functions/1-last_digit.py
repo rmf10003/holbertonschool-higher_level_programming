67e56e4d6c189b4f8e6b3dd6e64a92b281174caf
rmf10003 <685@holbertonschool.com>
2019-05-01 00:06:56 +0000

    safd

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,11 +4,16 @@ number = random.randint(-10000, 10000)
 print("Last digit of", end=' ')
 print(number, end=' is ')
 if number < 0:
-    number = -number
-print(number % 10, end=' ')
-if number % 10 > 5:
-    print('and is greater than 5')
-elif number % 10 == 0:
-    print('and is 0')
+    print(number % -10, end=' ')
+    if number % -10 == 0:
+        print('and is 0')
+    else:
+        print('and is less than 6 and not 0')
 else:
-    print('and is less than 6 and not 0')
+    print(number % 10, end=' ')
+    if number % 10 > 5:
+        print('and is greater than 5')
+    elif number % 10 == 0:
+        print('and is 0')
+    else:
+        print('and is less than 6 and not 0')
--- 0x01-python-if_else_loops_functions/8-main.py
+++ 0x01-python-if_else_loops_functions/8-main.py
@@ -3,3 +3,5 @@ uppercase = __import__('8-uppercase').uppercase
 
 uppercase("holberton")
 uppercase("Holberton School 98 Battery street")
+uppercase("sdafdsf 4532 FSAFf !@#%@#@&z;")
+uppercase("3423543243")
