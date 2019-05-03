342168bfd42d5e689306e97e5cfb32d5f1fda501
rmf10003 <685@holbertonschool.com>
2019-05-03 02:32:06 +0000

    finish mandatory

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == '__main__':
-    if len(argv - 1) == 1:
+    if len(argv) - 1 == 1:
         print("{} argument:".format(len(argv) - 1))
-    else
-        print("{} arguments:".format(len(argv) - 1), end="")
-        if len(argv - 1) == 0:
+    else:
+        print("{} arguments".format(len(argv) - 1), end="")
+        if len(argv) - 1 == 0:
             print(".")
         else:
             print(":")
-    for i in range(len(argv - 1)):
-            
+    for i in range(1, len(argv)):
+        print("{}: {}".format(i, argv[i]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+import inspect
+import hidden_4
+if __name__ == '__main__':
+    func = inspect.getmembers(hidden_4, inspect.isfunction)
+    for i in func:
+        print(i[0])
