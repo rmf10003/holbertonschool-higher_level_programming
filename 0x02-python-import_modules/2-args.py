c56b858fc4a62afe5f8e345cd1d5f196d0f5b80c
Rory <685@holbertonschool.com>
2019-05-03 00:37:53 +0000

    move comp

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == '__main__':
-    print(len(argv))
+    if len(argv - 1) == 1:
+        print("{} argument:".format(len(argv) - 1))
+    else
+        print("{} arguments:".format(len(argv) - 1), end="")
+        if len(argv - 1) == 0:
+            print(".")
+        else:
+            print(":")
+    for i in range(len(argv - 1)):
+            
