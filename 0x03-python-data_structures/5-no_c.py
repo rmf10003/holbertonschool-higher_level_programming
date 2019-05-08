2c1e1ce9312cde21df7b7b3d1ff6733d308b19a8
rmf10003 <685@holbertonschool.com>
2019-05-08 02:47:18 +0000

    all except 13

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    L2 = [char for char in my_string if char not in 'Cc']
+    return ''.join(L2)
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    # ['\n'.join([' '.join(str(L[i])) for i in L]) for L in matrix]
+    for row in matrix:
+        for i in range(len(row)):
+            if i == len(row) - 1:
+                print("{:d}".format(row[i]), end='')
+            else:
+                print("{:d}".format(row[i]), end=' ')
+        print()
