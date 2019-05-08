e68d22f80da2dbbccc29736de043684b06cefbaa
rmf10003 <685@holbertonschool.com>
2019-05-08 02:50:35 +0000

    remove comment

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    # ['\n'.join([' '.join(str(L[i])) for i in L]) for L in matrix]
     for row in matrix:
         for i in range(len(row)):
             if i == len(row) - 1:
