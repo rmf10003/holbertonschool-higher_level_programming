913d088a99419cf94b01131de652f7b38f8fdefb
rmf10003 <685@holbertonschool.com>
2019-05-10 01:58:14 +0000

    algorithim for roman numeral conversion

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
+def roman_to_int(rs):
+    if type(rs) is not str or rs is None:
+        return 0
+    conv = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    ival = 0
+    for l in range(len(rs)):
+        if l == 0 or conv[rs[l]] <= conv[rs[l]]:
+            ival += conv[rs[l]]
+        else:
+            ival += conv[rs[l]] - 2 * conv[rs[l - 1]]
+    return ival
