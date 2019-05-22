eccb972af27768a45e862a3cb61a5b1076fe10a2
rmf10003 <685@holbertonschool.com>
2019-05-22 02:10:40 +0000

    4 and 5

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,13 +11,22 @@ class Square(object):
     """
     def __init__(self, size=0):
         """Initialize class with attributes."""
-        if not isinstance(size, int):
+        self.__size = size
+    @property
+    def size(self):
+        """Gets the value of __size
+
+        Sets the value of __size
+        """
+        return self.__size
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
-
+            self.__size = value
     def area(self):
         """squares the size of the square.
 
