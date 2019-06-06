fab6791df7d433113cb75d7e9d066b7fce7b3dce
rmf10003 <685@holbertonschool.com>
2019-06-06 03:47:29 +0000

    square 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""mod docstr"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    """class docstr"""
+
+    def __init__(self, size):
+        """method docstr"""
+        super().integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """method docstr"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
+    def area(self):
+        """method docstr"""
+        return self.__size * self.__size
