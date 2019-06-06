845bdad428c753508aed37caedd6938e8499cf6b
rmf10003 <685@holbertonschool.com>
2019-06-06 03:42:18 +0000

    fds

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -8,9 +8,10 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """method docstr"""
-        super().super().integer_validator(size)
+        super().integer_validator("size", size)
         self.__size = size
+        super().__init__(size, size)
 
     def area(self):
         """method docstr"""
-        self.__size
+        return self.__size * self.__size
