901cb989e42012e6e9f2ba980388b0291043b8b6
rmf10003 <685@holbertonschool.com>
2019-06-07 12:52:12 +0000

    use readlines

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """fctn docstr"""
     with open(filename, 'r', encoding='utf-8') as f:
-        print(f.read())
+        for line in f.readlines():
+            print(line, end='')
