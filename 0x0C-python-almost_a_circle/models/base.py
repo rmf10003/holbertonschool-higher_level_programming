011a4ce8d668e2544a4f95509d74e94d5b1a38f5
rmf10003 <685@holbertonschool.com>
2019-06-12 17:15:11 +0000

    create method that returns an instance with all attributes preset

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,3 +33,23 @@ class Base:
                 l = [obj.to_dictionary() for obj in list_objs]
                 file.write(cls.to_json_string(l))
                     
+    @staticmethod
+    def from_json_string(json_string):
+        """return list of the JSON string repr"""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return an instance with all attributes already set"""
+        if cls.__name__ == 'Rectangle':
+            a = cls(4, 2)
+        elif cls.__name__ == 'Square':
+            a = cls(4)
+        a.update(**dictionary)
+        return a
+        
+    @classmethod
+    def load_from_file(cls):
+        """return a list of instances from a JSON file"""
