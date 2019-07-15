360fcd9778fbb5f60639a6fcc056659fd5bfc31d
rmf10003 <685@holbertonschool.com>
2019-07-15 21:10:56 +0000

    insert bacticks

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ name VARCHAR(256),
 score INT
 );
 -- INSERT INTO
-INSERT INTO second_table(id, name, score)
+INSERT INTO second_table(`id`, `name`, `score`)
 VALUES (1, "John", 10),
 (2, "ALEX", 3),
 (3, "Bob", 14),
