ce0913ab5adb6baa94900cac3ada1867868daf29
rmf10003 <685@holbertonschool.com>
2019-08-10 18:14:20 +0000

    heading to beach

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+''' module for state class'''
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declaritive_base
+
+Base = declarative_base()
+
+class State(Base):
+    """state class"""
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
