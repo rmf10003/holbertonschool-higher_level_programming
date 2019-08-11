ebc1311742c835edad9004946b006b4ef54541bb
rmf10003 <685@holbertonschool.com>
2019-08-11 01:38:26 +0000

    fix inputs

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 ''' module for state class'''
 
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declaritive_base
 
+
 Base = declarative_base()
+
 
 class State(Base):
     """state class"""
