b2ab3359cf1e459fb3b0dd77215cd4097c3c7934
rmf10003 <685@holbertonschool.com>
2019-08-12 00:04:15 +0000

    insert blankline

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from model_state import Base, State
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
+
 class City(Base):
     """City class"""
 
