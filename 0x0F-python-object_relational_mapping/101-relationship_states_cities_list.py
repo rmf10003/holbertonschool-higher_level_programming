1950ff85c6d77dc91338f9a4e890af583b080030
rmf10003 <685@holbertonschool.com>
2019-08-12 19:19:32 +0000

    finish advanced

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""script lists all State and corresponding City objects contained in the db"""
+
+from sys import argv
+from relationship_state import State
+from relationship_city import Base, City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(*argv[1:]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id)
+
+    for s in query:
+        print("{}: {}".format(s.id, s.name))
+        for c in s.cities:
+            print("\t{}: {}".format(c.id, c.name))
+
+    session.close()
--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""script lists all City objects from db"""
+
+from sys import argv
+from relationship_state import State
+from relationship_city import Base, City
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(*argv[1:]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(City).order_by(City.id)
+
+    for c in query:
+        print("{}: {} -> {}".format(c.id, c.name, c.state.name))
+
+    session.close()
