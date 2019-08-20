98ba582f05740c81b2eeb30c0a9590279e443b2d
rmf10003 <685@holbertonschool.com>
2019-08-20 19:08:51 +0000

    add -s option

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send POST request and display body of response with set vars
-curl "$1" -X POST -d "email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD"
+curl "$1" -X POST -s -d "email=hr@holbertonschool.com&subject=I+will+always+be+here+for+PLD"
