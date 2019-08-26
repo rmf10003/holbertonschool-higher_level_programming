522ac7a46402309cbb6f1bb43283c723aecd4658
rmf10003 <685@holbertonschool.com>
2019-08-26 20:04:55 +0000

    handle httperror codes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""take in URL, send a request to URL and display value of X-Reqeust-Id var"""
+import urllib.request
+import urllib.parse
+import urllib.error
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: " + str(e.code))
