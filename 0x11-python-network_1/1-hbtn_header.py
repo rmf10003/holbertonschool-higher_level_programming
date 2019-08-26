a3022c3d8a1788c5d83809b3cf6a858ea11b8ad2
rmf10003 <685@holbertonschool.com>
2019-08-26 19:01:08 +0000

    only execute if name is main

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,8 +3,8 @@
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
-with urllib.request.urlopen(url) as response:
-    html = response.read()
-    print(response.headers.get('X-Request-Id'))
+if __name__ == '__main__':
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print(response.headers.get('X-Request-Id'))
