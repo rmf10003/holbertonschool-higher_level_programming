c30b23c36f1701f7e31401536fa37752064d98ce
rmf10003 <685@holbertonschool.com>
2019-08-26 20:26:58 +0000

    use requests module to make a post request with email as a param

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""take in URL, send a request to URL and display value of X-Reqeust-Id var"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    r = requests.post(url, data = {'email': email})
+    print(r.text)
