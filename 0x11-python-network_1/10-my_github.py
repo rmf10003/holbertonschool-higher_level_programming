46fbb9d88a574c7a0ecb47ab4f3401c4dfbfc77e
rmf10003 <685@holbertonschool.com>
2019-08-26 22:49:24 +0000

    get id finished

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""take in a string argument and use requests module to query starwars api"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "https://api.github.com/user"
+    r = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
+    j_dict = r.json()
+    print(j_dict.get('id'))
