b6e4d8a4feecf0f24c8b085e9dd256008b303de7
rmf10003 <685@holbertonschool.com>
2019-08-26 18:58:31 +0000

    take in URL, send a request to URL and display value of X-Reqeust-Id var

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""take in URL, send a request to URL and display value of X-Reqeust-Id var"""
+import urllib.request
+import sys
+
+
+url = sys.argv[1]
+with urllib.request.urlopen(url) as response:
+    html = response.read()
+    print(response.headers.get('X-Request-Id'))
