e2f9a9fdc3ee8c8182e49a5f5671abd83216eedd
rmf10003 <685@holbertonschool.com>
2019-08-26 18:30:05 +0000

    fetch content from url using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,5 +5,6 @@ import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
-    for item in html:
-        print(item)
+    print("Body response:\n\t- type: " + str(type(html)))
+    print("\t- content: " + str(html))
+    print("\t- utf8 content: " + html.decode("utf-8"))
