0c72cef83256704103832dc5ad2b4305788953fc
rmf10003 <685@holbertonschool.com>
2019-08-26 22:37:53 +0000

    write a python script that can take in a string argument and use requests module to query starwars api

--- 0x11-python-network_1/9-starwars.py
+++ 0x11-python-network_1/9-starwars.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""take in a string argument and use requests module to query starwars api"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = "https://swapi.co/api/people"
+    search = sys.argv[1]
+    r = requests.get(url, params={'search': search})
+    j_dict = r.json()
+    print("Number of results: {}".format(j_dict.get('count')))
+    for character in j_dict.get('results'):
+        print(character.get('name'))
