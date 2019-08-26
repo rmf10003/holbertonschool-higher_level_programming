9b30fb555428c147d28fdd23b479b3d201725cd9
rmf10003 <685@holbertonschool.com>
2019-08-26 20:45:19 +0000

    use requests module to fetch a page given a url and handle error code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python3                                                           
+"""use requests module to fetch a page given a url and handle error code"""
+import requests
+import sys
+
+r = requests.get(sys.argv[1])
+if r.status_code >= 400:
+    print("Error code: " + str(r.status_code))
+else:
+    print(r.text)
