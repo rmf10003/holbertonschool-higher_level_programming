59065567ff979d5a00165977d18a80a419b507b4
rmf10003 <685@holbertonschool.com>
2019-08-26 19:44:39 +0000

    py script to take url and email addr and send post reqeust with email as a param

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""take in URL, send a request to URL and display value of X-Reqeust-Id var"""
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    try:
+        req = urllib.request.Request(url, data)
+        with urllib.request.urlopen(req) as response:
+            html = response.read()
+            print(html.decode('utf-8'))
+    except Exception as e:
+        print(e)
