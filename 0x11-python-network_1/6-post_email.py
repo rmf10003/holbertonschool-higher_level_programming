28b171b1bbb0400b00507fd6182e8dcde09e6719
rmf10003 <685@holbertonschool.com>
2019-08-26 20:49:26 +0000

    fix pep8 compliance

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -6,5 +6,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-    r = requests.post(url, data = {'email': email})
+    r = requests.post(url, data={'email': email})
     print(r.text)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3                                                           
+#!/usr/bin/python3
 """use requests module to fetch a page given a url and handle error code"""
 import requests
 import sys
 
-r = requests.get(sys.argv[1])
-if r.status_code >= 400:
-    print("Error code: " + str(r.status_code))
-else:
-    print(r.text)
+if __name__ is '__main__':
+    r = requests.get(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: " + str(r.status_code))
+    else:
+        print(r.text)
