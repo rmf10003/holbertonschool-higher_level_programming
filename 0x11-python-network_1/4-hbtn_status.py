5ca8d2b2c5fa4a3ceb74dea0dea9581a91c43b39
rmf10003 <685@holbertonschool.com>
2019-08-26 20:16:35 +0000

    use request module to fetch from a url

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""use requests module to fetch a page given a url"""
+import requests
+
+r = requests.get('https://intranet.hbtn.io/status')
+print("Body response:\n\t- type: " + str(type(r.text)))
+print("\t- content: " + str(r.text))
