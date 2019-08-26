83f5f157b7df459ce2cf03f5e6b8100acfbb61d5
rmf10003 <685@holbertonschool.com>
2019-08-26 20:20:45 +0000

    use requests module to return value of header

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""take in URL, send a request to URL and display value of X-Reqeust-Id var"""
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    r = requests.get(url)
+    print(r.headers.get('x-Request-Id'))
