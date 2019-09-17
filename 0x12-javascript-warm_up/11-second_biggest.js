27ffa99517e69d4583ef0c82300134e81db8af62
rmf10003 <685@holbertonschool.com>
2019-09-17 20:02:33 +0000

    change string printout and handle edge case for negative numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,13 @@ if (argv.length === 0 || argv.length === 1) {
   console.log(0);
   process.exit();
 }
-let second = 0; let first = argv[0];
+let second = argv[1];
+let first = argv[0];
+if (second > first) {
+  second ^= first;
+  first ^= second;
+  second ^= first;
+}
 for (let i = 0; i < argv.length; i++) {
   if (argv[i] > first) {
     second = first;
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,5 +4,5 @@ if (isNaN(x)) {
   console.log('Missing number of occurrences');
 }
 for (let i = 0; i < x; i++) {
-  console.log('C is Fun');
+  console.log('C is fun');
 }
