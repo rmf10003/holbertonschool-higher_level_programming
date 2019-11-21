9cd67be846e628f259610cc3f4a102816a371334
Rory Fahy <685@holbertonschool.com>
2019-11-21 13:20:08 -0800

    Update 0-print_list_integer.py
    add docstring
    make code more pythonic

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""print list integer"""
 def print_list_integer(my_list=[]):
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    """print a list of integers to the standard out"""
+    for num in my_list:
+        print("{:d}".format(num))
